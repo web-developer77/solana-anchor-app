4928de56d3383e83ddacc27e6e6ce846593909b0
web-developer77 <koa06800@gmail.com>
2021-11-30 15:11:57 -0800

    account contstraints and access control

---
+++ programs/example/src/lib.rs
@@ -0,0 +1,42 @@
+use anchor_lang::prelude::*;
+
+declare_id!("J3Aj8wXApsq3A7H2Yax7w1KYWJBprTCTzNeEiyVA2VJP");
+
+#[program]
+pub mod example {
+    use super::*;
+
+    pub fn create(ctx: Context<Create>, authority: Pubkey) -> ProgramResult {
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = authority;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 8 + 40)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub data: u64,
+}
